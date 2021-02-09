3e3f86505a63f1332d4b49eb19b2f3cf9faf5526
podrubatov <45881203+podrubatov@users.noreply.github.com>
2021-02-09 14:49:04 +0300

    Create autoaccept.js

---
+++ autoaccept.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         AUTO ACCEPT 4 FASTCUP.NET
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  https://steamcommunity.com/id/Podrubatov/
+// @author       Podrubatov
+// @match        https://fastcup.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+setInterval( () => {
+    let btn = document.getElementsByClassName("oRTKZ 2dQ- _27M3P");
+    if( btn.length ) {
+        btn[0].click();
+    }
+}, 2000 )
+    // Your code here...
+})();
