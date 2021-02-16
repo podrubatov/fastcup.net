2ad09bea99a0073d98d3c10377ad75881555655d
podrubatov <45881203+podrubatov@users.noreply.github.com>
2021-02-16 22:17:00 +0300

    Update faceitLink.user.js

--- faceitLink.user.js
+++ faceitLink.user.js
@@ -11,22 +11,23 @@
 //Если не отображается кнопка обновите страницу
 (function() {
     'use strict';
+    let st = null;
 	document.addEventListener("DOMSubtreeModified", function() {
-
-		let parent = $('div._3cgCe');
-		let currentUrl = window.location.href;
-		let steamUrl = $('._2uQMM > a:first-child');
-
-		if( currentUrl.includes("fastcup") && parent.length && steamUrl.length ) {
-			let steamid = String(steamUrl.attr('href')).split('/').pop();
-			let btnid = 'steam-' + steamid;
-			if( !$(btnid).length ) {
-				let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
-				let faceit = $(`<div id="${btnid}" class="_2uQMM add-faceit-btn"><a href="${urlFaceit}" target="_blank" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
-				parent.find('.add-faceit-btn').remove();
-        		parent.append(faceit);
+		clearTimeout(st)
+		st = setTimeout( () => {
+			let parent = $('div._3cgCe');
+			let currentUrl = window.location.href;
+			let steamUrl = $('._2uQMM > a:first-child');
+			if( currentUrl.includes("fastcup") && parent.length && steamUrl.length ) {
+				let steamid = String(steamUrl.attr('href')).split('/').pop();
+				let btnid = 'steam-' + steamid;
+				if( !$('#'+ btnid).length ) {
+					let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
+					let faceit = $(`<div id="${btnid}" class="_2uQMM add-faceit-btn"><a href="${urlFaceit}" target="_blank" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
+					parent.find('.add-faceit-btn').remove();
+	        		parent.append(faceit);
+				}
 			}
-		}
-
+	    }, 200)
 	});
 })();
