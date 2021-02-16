55f2f0ad7ef67924d1ea320868581a7b26d6b42d
podrubatov <45881203+podrubatov@users.noreply.github.com>
2021-02-16 21:47:19 +0300

    Update faceitLink.user.js

--- faceitLink.user.js
+++ faceitLink.user.js
@@ -11,19 +11,17 @@
 //Если не отображается кнопка обновите страницу
 (function() {
     'use strict';
- let interval = setInterval( () => {
-        let parent = $('div._3cgCe');
-        if( parent.length ) {
-            const currentUrl = window.location.href;
-            if(currentUrl.includes("fastcup")) {
-                const steamlink = String($('._2uQMM > a:first-child').attr('href'));
-                const steamid = steamlink.slice(36);
-                const urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
-                const faceit = $(`<div class="_2uQMM"><a href="${urlFaceit}" target="_blank" id="knopka" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
-        parent.append(faceit);
-        clearTimeout(interval);
-}}}, 200)
-    setTimeout( () => {
-        clearTimeout(interval)
-    }, 5000)
+	document.addEventListener("DOMSubtreeModified", function() {
+		let parent = $('div._3cgCe');
+		if( parent.length && !$('#inner_btn').length ) {
+			let currentUrl = window.location.href;
+			if(currentUrl.includes("fastcup")) {
+				let steamlink = String($('._2uQMM > a:first-child').attr('href')).slice(36);
+				let steamid = steamlink.slice(36);
+				let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
+				let faceit = $(`<div id="inner_btn" class="_2uQMM"><a href="${urlFaceit}" target="_blank" id="knopka" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
+	        	parent.append(faceit);
+			}
+		}
+	});
 })();
