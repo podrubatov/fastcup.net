8c8db518dcf8e80819e16180b2deee82b4ca2f0f
podrubatov <45881203+podrubatov@users.noreply.github.com>
2021-02-16 21:57:09 +0300

    Update faceitLink.user.js

--- faceitLink.user.js
+++ faceitLink.user.js
@@ -12,13 +12,15 @@
 (function() {
     'use strict';
 	document.addEventListener("DOMSubtreeModified", function() {
+
 		let parent = $('div._3cgCe');
 		let currentUrl = window.location.href;
-		if( currentUrl.includes("fastcup") && parent.length && !$('#inner_btn').length ) {
-			let steamlink = String($('._2uQMM > a:first-child').attr('href')).slice(36);
-			let steamid = steamlink.slice(36);
-			let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
-			let faceit = $(`<div id="inner_btn" class="_2uQMM"><a href="${urlFaceit}" target="_blank" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
+
+		let steamid = String($('._2uQMM > a:first-child').attr('href')).split('/').pop();
+		let btnid = 'steam-' + steamid;
+
+		if( currentUrl.includes("fastcup") && parent.length && steamid.length && !$(btnid).length ) {
+			let faceit = $(`<div id="${btnid}" class="_2uQMM"><a href="${urlFaceit}" target="_blank" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
         	parent.append(faceit);
 		}
 	});
