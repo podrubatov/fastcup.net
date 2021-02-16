83181660875ed0a27ee35c4cb077d7255d779f76
podrubatov <45881203+podrubatov@users.noreply.github.com>
2021-02-16 21:48:22 +0300

    Update faceitLink.user.js

--- faceitLink.user.js
+++ faceitLink.user.js
@@ -13,15 +13,13 @@
     'use strict';
 	document.addEventListener("DOMSubtreeModified", function() {
 		let parent = $('div._3cgCe');
-		if( parent.length && !$('#inner_btn').length ) {
-			let currentUrl = window.location.href;
-			if(currentUrl.includes("fastcup")) {
-				let steamlink = String($('._2uQMM > a:first-child').attr('href')).slice(36);
-				let steamid = steamlink.slice(36);
-				let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
-				let faceit = $(`<div id="inner_btn" class="_2uQMM"><a href="${urlFaceit}" target="_blank" id="knopka" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
-	        	parent.append(faceit);
-			}
+		let currentUrl = window.location.href;
+		if( currentUrl.includes("fastcup") && parent.length && !$('#inner_btn').length ) {
+			let steamlink = String($('._2uQMM > a:first-child').attr('href')).slice(36);
+			let steamid = steamlink.slice(36);
+			let urlFaceit = `https://faceitfinder.com/profile/${steamid}`;
+			let faceit = $(`<div id="inner_btn" class="_2uQMM"><a href="${urlFaceit}" target="_blank" rel="noopener noreferrer" class="_33xKn _1Az77"><span>FACEIT</span></a></div>`);
+        	parent.append(faceit);
 		}
 	});
 })();
